0b7f38bbd4d65394a9f680851e3eaf9284c80342
Guillermo Rodriguez <dg.guillermo.rodriguez@gmail.com>
2020-10-10 12:58:52 -0300

    Agregado de iconos en seccion beneficios

--- src/components/Benefits.js
+++ src/components/Benefits.js
@@ -1,7 +1,9 @@
 import React from "react";
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDollarSign } from "@fortawesome/free-solid-svg-icons";
+import iconoDisponibilidad from "../assets/statics/icono_disponibilidad.svg";
+import iconoFinanciacion from "../assets/statics/icono_financiacion.svg";
+import iconoPagoSoja from "../assets/statics/icono_pago_con_soja.svg";
+import iconoPostVenta from "../assets/statics/icono_servicio_posventa.svg";
 
 const Benefits = (props) => (
   <div className="benefits">
@@ -10,7 +12,7 @@ const Benefits = (props) => (
         <div className="col-md-3">
           <div className="benefits-item">
             <div className="benefits-item-icon">
-              <FontAwesomeIcon icon={faDollarSign} size="4x" fixedWidth />
+              <img className="img-fluid" src={iconoDisponibilidad} alt="ícono disponibilidad inmediata"/>
             </div>
             <div className="benefits-item-title">Disponibilidad inmediata</div>
           </div>
@@ -18,7 +20,7 @@ const Benefits = (props) => (
         <div className="col-md-3">
           <div className="benefits-item">
             <div className="benefits-item-icon">
-              <FontAwesomeIcon icon={faDollarSign} size="4x" fixedWidth />
+              <img className="img-fluid" src={iconoFinanciacion} alt="ícono financiación Semtraco"/>
             </div>
             <div className="benefits-item-title">
                 Financiación Semtraco hasta 36 meses
@@ -28,7 +30,7 @@ const Benefits = (props) => (
         <div className="col-md-3">
           <div className="benefits-item">
             <div className="benefits-item-icon">
-              <FontAwesomeIcon icon={faDollarSign} size="4x" fixedWidth />
+              <img className="img-fluid" src={iconoPagoSoja} alt="ícono pago con soja"/>
             </div>
             <div className="benefits-item-title">
             Pago total con Soja
@@ -38,7 +40,7 @@ const Benefits = (props) => (
         <div className="col-md-3">
           <div className="benefits-item">
             <div className="benefits-item-icon">
-              <FontAwesomeIcon icon={faDollarSign} size="4x" fixedWidth />
+              <img className="img-fluid" src={iconoPostVenta} alt="ícono post venta Semtraco"/>
             </div>
             <div className="benefits-item-title">
             Servicio Post Venta Semtraco
